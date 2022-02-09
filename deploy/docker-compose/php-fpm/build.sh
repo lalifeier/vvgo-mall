32d3cbd8f129ff901bcfd8e99943c3e6e233f6cf
lalifeier <lalifeier@gmail.com>
2022-02-09 23:50:17 +0800

    init

--- deploy/docker-compose/php-fpm/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# docker cp php-fpm:/usr/local/etc/php-fpm.d/www.conf www.conf
-docker cp php-fpm:/etc/php/8.1/fpm/php-fpm.conf www.conf
-docker cp php-fpm:/usr/lib/php/8.1/php.ini-production php.ini
